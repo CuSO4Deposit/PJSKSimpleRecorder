9df1e66bf8d8fd8401f80a00501177052ec481da
CuSO4_Deposit <CuSO4D@protonmail.com>
2023-09-14 04:20:42 +0000

    get_song_info tested.

--- modules/utils.py
+++ modules/utils.py
@@ -1,6 +1,39 @@
+from contextlib import closing
 import httpx
+import json
 from loguru import logger
+from pathlib import Path
 import sqlite3
+
+
+def insert_into_db(user: str, song_id: int, song_name: str, difficulty: str, perfect: int, great: int, good: int, bad: int, miss: int, time: int):
+    db_path = Path(__file__).parent.parent / "database" / "pjsk.db"
+    if not db_path.exists():
+        db_path.parent.mkdir(exist_ok=True)
+        with closing(sqlite3.connect(db_path)) as con:
+            with con: 
+                con.execute("""\
+                    CREATE TABLE record(
+                        [song_id] INT NOT NULL,
+                        [song_name] TEXT NOT NULL,
+                        [difficulty] TEXT NOT NULL,
+                        [perfect] INT NOT NULL,
+                        [great] INT NOT NULL,
+                        [good] INT NOT NULL,
+                        [bad] INT NOT NULL,
+                        [miss] INT NOT NULL,
+                        [time] INT NOT NULL,
+                        [user] TEXT NOT NULL,
+                        PRIMARY KEY([time], [user])
+                    );"""
+                )
+    with closing(sqlite3.connect(db_path)) as con:
+        with con:
+            con.execute("""
+                INSERT INTO record ([song_id], [song_name], [difficulty], [perfect], [great], [good], [bad], [miss], [time], [user])
+                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)""", 
+                (song_id, song_name, difficulty, perfect, great, good, bad, miss, time, user,)
+            )
 
 
 def get_response(url: str, headers=None):
@@ -28,3 +61,29 @@ def get_song_id(alias: str):
         return response["title"], response["musicId"], response["match"]
     else:
         return False
+
+
+def get_song_info(song_id: int, difficulty: str):
+    musicsjson = Path(__file__).parent.parent / "database" / "musics.json"
+    musicDifficultiesjson = Path(__file__).parent.parent / "database" / "musicDifficulties.json"
+    info = {}
+
+    with open(musicsjson) as f:
+        result = json.load(f)
+        for i in result:
+            if i["id"] != song_id:
+                continue
+            info["title"] = i["title"]
+            break
+
+    with open(musicDifficultiesjson) as f:
+        result = json.load(f)
+        for i in result:
+            if i["musicId"] == song_id and i["musicDifficulty"] == difficulty:
+                info["playLevel"] = i["playLevel"]
+                info["totalNoteCount"] = i["totalNoteCount"]
+
+    info["musicId"] = song_id
+    info["musicDifficulty"] = difficulty
+
+    return info
--- test/test_pjskutils.py
+++ test/test_pjskutils.py
@@ -2,7 +2,16 @@ from modules import utils as pjsk
 
 
 def test_get_song_id():
-    r = pjsk.get_song_id("消失")
-    assert r[0] == "初音ミクの消失"
-    assert r[1] == 49
+    r = pjsk.get_song_id("皇帝")
+    assert r[0] == "the EmpErroR"
+    assert r[1] == 163
     assert r[2] == 1
+
+
+def test_get_song_info():
+    info = pjsk.get_song_info(163, "master")
+    assert info["title"] == "the EmpErroR"
+    assert info["playLevel"] == 36
+    assert info["totalNoteCount"] == 1593
+    assert info["musicId"] == 163
+    assert info["musicDifficulty"] == "master"
